edc87acd8f5942a1649b07e1ad6a1df6f16ecc13
Vaisvik-Chaudhary <98399224+Vaisvik-Chaudhary@users.noreply.github.com>
2022-02-09 12:32:50 +0530

    Update Practical-1.1.js

--- Practical-1/Practical-1.1.js
+++ Practical-1/Practical-1.1.js
@@ -1,5 +1,5 @@
 const numbers = [8,5,9,3,5];
-const average = numbers.reduce((firstnum,lastnum) => {
+const sum = numbers.reduce((firstnum,lastnum) => {
     return firstnum + lastnum;
 })
-console.log(average/numbers.length);
+console.log(sum/numbers.length);
